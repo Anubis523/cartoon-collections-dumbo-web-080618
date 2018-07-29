0ebacc04bd8e760456b293e39df26deb6c64c4aa
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-29 19:17:02 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves (dwarves)# code an argument here
   dwarves.each_with_index{|dwarf, index| puts "#{index+1}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument hereS
+def summon_captain_planet (powers)# code an argument hereS
   # Your code here
 end
 
