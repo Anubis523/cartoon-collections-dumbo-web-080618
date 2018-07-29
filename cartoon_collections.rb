9a8e15fb5638e1b7326515c513118c15a457faff
Kevin Steele <kevindsteeleii@gmail.com>
2018-07-29 19:28:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,5 +16,5 @@ end
 def find_the_cheese (items)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  items.find?{|item| cheese_types.include?(item)}
+  items.find{|item| cheese_types.include?(item)}
 end
